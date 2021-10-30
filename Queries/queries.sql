a1c91acce8b6cc174d4ad385f309c9a4d2dad230
Suvigya Jain <suvigyajain1@gmail.com>
2021-10-30 13:56:51 +0530

    grANTS

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,30 +1,11 @@
+-- ==================================
+-- SIMPLE QUERIES
+-- -----------------------------------
 
--- Get voters list per constituency
-select * from voter
-where constituency_id in 
-    (
-        select constituency_id from constituency
-        where name='Mysore' and type='legislative'
-    );
 
 -- Get voter list overall
 select * from voter ;
 
--- Get voter turnout
-select (count_cand*100.0) / (count_voter) as turnout
-from (
-    select count(candidate_id) as count_cand, count(voter_id) as count_voter 
-    from voter
-    ) as counts;
-
--- Voter turnout per LS constituency
-select (count_cand*100.0) / (count_voter) as turnout
-from (
-    select count(candidate_id) as count_cand, count(voter_id) as count_voter 
-    from voter
-    group by constituency_id
-    ) as counts
-;
 -- Cast vote
 update voter set candidate_id='cand_001' where voter_id='vot_001';
 
@@ -50,6 +31,12 @@ where c.party_id = p.party_id and p.coalition_id='coal_03_19';
 -- Get coalitions
 select * from coalition_id where election_id='el_ls_01';
 
+
+
+-- =========================================================
+-- COMPLEX QUERIES
+-- ---------------------------------------------------------
+
 -- Get votes by constituency
 select candidate.candidate_id, voter_id
 from voter
@@ -69,20 +56,85 @@ select votes.candidate_id, count(votes.voter_id) as num_votes from
 group by votes.candidate_id
 
 -- Find winning candidate for given constituency
-select distinct on (counts.num_votes) candidate_id, num_votes
-from
+select votes.candidate_id, count(votes.voter_id) as num_votes from 
 (
-    select votes.candidate_id, count(votes.voter_id) as num_votes from 
+    select candidate.candidate_id, voter_id
+    from voter
+    full outer join candidate
+    on voter.candidate_id = candidate.candidate_id
+    where candidate.constituency_id='con_ls_001'
+) as votes
+group by votes.candidate_id
+order by num_votes DESC
+limit 1;
+
+-- Get voters list per constituency
+select * from voter
+where constituency_id in 
     (
-        select candidate.candidate_id, voter_id
-        from voter
-        full outer join candidate
-        on voter.candidate_id = candidate.candidate_id
-        where candidate.constituency_id='con_ls_001'
-    ) as votes
-    group by votes.candidate_id
-) as counts
-order by counts.num_votes DESC;
+        select constituency_id from constituency
+        where name='Mysore' and type='legislative'
+    );
+
+
+-- Get voter turnout
+select (count_cand*100.0) / (count_voter) as turnout
+from (
+    select count(candidate_id) as count_cand, count(voter_id) as count_voter 
+    from voter
+    ) as counts;
+
+-- Voter turnout per LS constituency
+select (count_cand*100.0) / (count_voter) as turnout
+from (
+    select count(candidate_id) as count_cand, count(voter_id) as count_voter 
+    from voter
+    group by constituency_id
+    ) as counts
+;
 -- Find winning party for given state
 -- Find winning coalition in the state
 -- Find winning coalition in the lok sabha election
+
+-- USER PRIVILEGES
+
+Users:
+    Election Commission:
+        CRUD election
+        See statistics (R all)
+        CRUD constituency
+
+    Booth:
+        U voter 
+        R list of candidates for their const
+
+    Booth Officer:
+        R voter list
+        R voter turnout
+        R candidate list
+
+    Electoral Registration Officer:
+        CRUD voter
+        CRUD candidate
+        CRUD party
+        CRUD coalition
+
+
+create user ec with password 'ec';
+create user booth with password 'booth';
+create user bo with password 'bo';
+create user ero with password 'ero';
+
+grant insert, select, update, delete on election to ec;
+grant select on all tables in schema public to ec;
+grant insert, select, update, delete on constituency to ec;
+
+grant select, update on voter to booth;
+grant select on candidate to booth;
+
+grant select on voter, candidate to bo;
+
+grant insert, select, update, delete on voter to ero;
+grant insert, select, update, delete on candidate to ero;
+grant insert, select, update, delete on party to ero;
+grant insert, select, update, delete on coalition to ero;
