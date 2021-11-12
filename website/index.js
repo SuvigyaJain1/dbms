0bd31e6c920f5edaca5012555008468d41411350
Suvigya Jain <suvigyajain1@gmail.com>
2021-11-12 22:17:56 +0530

    fetch post not working

--- website/index.js
+++ website/index.js
@@ -18,21 +18,50 @@ function getElectionDetails() {
 }
 
 function getSeatsPerParty() {
-    let tally = {}; 
-    fetch("http://localhost:5000/constituency/kar_01")
+    const e_id = 'kar_01'
+    fetch(`http://localhost:5000/constituency/${e_id}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        // data.map(constituency => {
-        //     const_id = constituency.constituency_id;
-        //     fetch(`http://localhost:5000/winnerConstituency/voter_kar_01/${const_id}`)
-        //     .then(res => res.json())
-        //     .then(winner => {
-        //         console.log(winner);
-                // const c_id = winner[0].candidate_id;
-                // const num_votes = winner[0].num_votes;
-                // console.log(candidate_id, num_votes);
-    //         })
-    //     })
+        let candidates = [];
+        data.map(constituency => {
+            const_id = constituency.constituency_id;
+            fetch(`http://localhost:5000/winnerConstituency/voter_${e_id}/${const_id}`)
+            .then(res => res.json())
+            .then(winner => {
+                // console.log(winner);
+                const c_id = winner[0].candidate_id;
+                const num_votes = winner[0].num_votes;
+                candidates.push(c_id);
+            })
+        })
+        console.log(candidates)
+        return candidates
+    })
+    .then((candidates) => {
+        let tally = {}; 
+        const payload = {}
+        payload.candidates = candidates
+
+        console.log(candidates)
+        fetch("http://localhost:5000/get_candidates_party", 
+        {
+            method:'POST',
+            // headers: {
+            //     'Accept': 'application/json',
+            //     'Content-Type': 'application/json'
+            // },
+            body: JSON.stringify(payload)
+        })
+        .then(res => res.json())
+        .then(parties => {
+            console.log(parties, typeof(parties))
+            parties.map(party => {
+                pid = party.party_id;
+                if(tally.hasOwnProperty(pid)) tally[pid] += 1;
+                else tally[pid] = 1
+            })
+
+            console.log(tally)
+        })
     })
 }
